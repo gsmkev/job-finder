c2b2dc3ac160e8374f991029fb83aa6dd7fdc4a7
gsmkev <gsmkev@gmail.com>
2024-12-04 13:46:45 -0300

    feat: add JobCategory component to HomePage
    - Added `JobCategory` component to `HomePage` to display job categories.
    - Implemented a carousel for job categories in `JobCategory` component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import {createTheme, MantineProvider} from '@mantine/core';
 import '@mantine/core/styles.css';
+import '@mantine/carousel/styles.css';
 import HomePage from "./Pages/HomePage";
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
