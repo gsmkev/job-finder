da2aa58b643deea185d60e30a86ccc2122fd28c7
gsmkev <gsmkev@gmail.com>
2024-12-04 12:31:53 -0300

    feat: add Companies component to HomePage
    - Added `Companies` component to `HomePage` to display trusted companies.
    - Implemented a marquee effect for company logos in `Companies` component.

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import Header from '../Header/Header';
 import DreamJob from "../LandingPage/DreamJob";
+import Companies from "../LandingPage/Companies";
 
 const HomePage = () => {
     return (
         <div className={'min-h-[100vh] bg-mine-shaft-950 font-[poppins]'}>
             <Header />
             <DreamJob />
+            <Companies />
         </div>
 
     );
