7e3caa398c819bf9878ad202dbd441897ed8ecfc
gsmkev <gsmkev@gmail.com>
2024-12-05 10:18:28 -0300

    refactor: Update navigation link text
    - Changed 'Find Talent' to 'Find Talents' in navigation links in `frontend/src/Header/NavLinks.tsx`.

--- frontend/src/Header/NavLinks.tsx
+++ frontend/src/Header/NavLinks.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 const NavLinks = () => {
     const links = [
         { name: 'Find Jobs', url: '/find-jobs' },
-        { name: 'Find Talent', url: '/find-talent' },
+        { name: 'Find Talents', url: '/find-talents' },
         { name: 'Upload Job', url: '/upload-job' },
         { name: 'About Us', url: '/about-us' },
     ];
