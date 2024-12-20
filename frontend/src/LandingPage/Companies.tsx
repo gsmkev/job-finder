7df362df139c7f37bf217daf45d1e0fc6894708b
gsmkev <gsmkev@gmail.com>
2024-12-20 20:36:37 -0300

    style: enhance code readability in Companies and DreamJob components

--- frontend/src/LandingPage/Companies.tsx
+++ frontend/src/LandingPage/Companies.tsx
@@ -11,9 +11,9 @@ const Companies = () => {
                 {companies.map((company, index) =>
 
                     <div key={index} className={'mx-8 px-2 py-1 hover:bg-mine-shaft-900 rounded-xl cursor-pointer'}>
-                        <img className={'h-14'} src={`/companies/${company}.png`} alt={company}/>
+                        <img className={'h-14'} src={`/companies/${company}.png`} alt={company} />
                     </div>
-                ) }
+                )}
             </Marquee>
         </div>
     )
