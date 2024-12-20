3e1ce231c4c67b5a0855b80646bf99bb04d70d93
gsmkev <gsmkev@gmail.com>
2024-12-20 19:46:51 -0300

    refactor: Improve Login and Register components structure
    - Extract icon constants in both components for better reusability
    - Format form inputs props for better readability

--- frontend/src/Auth/Login.tsx
+++ frontend/src/Auth/Login.tsx
@@ -3,13 +3,32 @@ import { IconAt, IconLock } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    
+    const iconAt = <IconAt style={{width: rem(16), height: rem(16)}} />;
+    const iconLock = <IconLock style={{width: rem(16), height: rem(16)}} />;
+
     return (
         <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
             <div className="text-2xl font-semibold">Login Account</div>
-            <TextInput label="Email" placeholder="Enter your email" withAsterisk leftSection={<IconAt style={{width: rem(16), height: rem(16)}} />} />
-            <PasswordInput leftSection={<IconLock style={{width: rem(16), height: rem(16)}} />} label="Password" placeholder="Enter your password" withAsterisk />
+            <TextInput 
+                withAsterisk 
+                leftSection={iconAt} 
+                label="Email" 
+                placeholder="Enter your email" 
+            />
+            <PasswordInput 
+                withAsterisk
+                leftSection={iconLock}
+                label="Password" 
+                placeholder="Enter your password"
+            /> 
             <Button autoContrast variant="filled" className="">Register</Button>
-            <div className="mx-auto">Don't have an account? <Link className="text-bright-sun-400 hover:underline font-medium" to={'/register'}>Register here</Link></div>
+            <div className="mx-auto">
+                Don't have an account? 
+                <Link className="text-bright-sun-400 hover:underline font-medium" to={'/register'}>
+                    Register here
+                </Link>
+            </div>
         </div>
     );
 }
--- frontend/src/Auth/Register.tsx
+++ frontend/src/Auth/Register.tsx
@@ -3,16 +3,44 @@ import { IconAt, IconLock } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+
+    const iconAt = <IconAt style={{width: rem(16), height: rem(16)}} />;
+    const iconLock = <IconLock style={{width: rem(16), height: rem(16)}} />;
+
     return (
         <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
             <div className="text-2xl font-semibold">Register Account</div>
-            <TextInput label="Full Name" placeholder="Enter your name" withAsterisk />
-            <TextInput label="Email" placeholder="Enter your email" withAsterisk leftSection={<IconAt style={{width: rem(16), height: rem(16)}} />} />
-            <PasswordInput leftSection={<IconLock style={{width: rem(16), height: rem(16)}} />} label="Password" placeholder="Enter your password" withAsterisk />
-            <PasswordInput leftSection={<IconLock style={{width: rem(16), height: rem(16)}} />} label="Confirm Password" placeholder="Confirm your password" withAsterisk />
+            <TextInput 
+                withAsterisk
+                label="Full Name" 
+                placeholder="Enter your name"
+            /> 
+            <TextInput 
+                withAsterisk 
+                leftSection={iconAt}
+                label="Email" 
+                placeholder="Enter your email"
+            /> 
+            <PasswordInput 
+                withAsterisk 
+                leftSection={iconLock} 
+                label="Password" 
+                placeholder="Enter your password" 
+            />
+            <PasswordInput 
+                withAsterisk 
+                leftSection={iconLock} 
+                label="Confirm Password" 
+                placeholder="Confirm your password" 
+            />
             <Checkbox autoContrast label={<>I agree to the <Anchor>terms and conditions</Anchor></>} />
             <Button autoContrast variant="filled" className="">Register</Button>
-            <div className="mx-auto">Already have an account? <Link className="text-bright-sun-400 hover:underline font-medium" to={'/login'}>Sign in here</Link></div>
+            <div className="mx-auto">
+                Already have an account? 
+                <Link className="text-bright-sun-400 hover:underline font-medium" to={'/login'}>
+                    Sign in here
+                </Link>
+            </div>
         </div>
     );
 }
