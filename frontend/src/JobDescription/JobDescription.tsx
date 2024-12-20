5467cdb69a7552319c8d591a36518a95ab24145d
gsmkev <gsmkev@gmail.com>
2024-12-20 20:24:55 -0300

    style: improve code readability in JobDescription component

--- frontend/src/JobDescription/JobDescription.tsx
+++ frontend/src/JobDescription/JobDescription.tsx
@@ -30,7 +30,13 @@ const JobDescription = (props:any) => {
             <div className="flex justify-between">
                 {card.map((item: any, index: number) =>(
                     <div key={index} className="flex flex-col items-center gap-1">
-                        <ActionIcon color="bright-sun.4" className="!h-12 !w-12" variant="light" radius="xl" aria-label="Settings">
+                        <ActionIcon 
+                            color="bright-sun.4" 
+                            className="!h-12 !w-12" 
+                            variant="light" 
+                            radius="xl" 
+                            aria-label="Settings"
+                        >
                             <item.icon className="h-4/5 w-4/5" stroke={1.5} />
                         </ActionIcon>
                         <div className="text-sm text-mine-shaft-300">{item.name}</div>
@@ -43,14 +49,34 @@ const JobDescription = (props:any) => {
                 <div className="text-xl font-semibold mb-5">Required Skills</div>
                 <div className="flex flex-wrap gap-2">
                     {skills.map((item:any, index:number) => (
-                        <ActionIcon key={index} color="bright-sun.4" className="!h-fit !w-fit font-medium !text-sm" variant="light" p="xs" radius="xl" aria-label="Settings">
+                        <ActionIcon 
+                            key={index} 
+                            color="bright-sun.4" 
+                            className="!h-fit !w-fit font-medium !text-sm" 
+                            variant="light" 
+                            p="xs" 
+                            radius="xl" 
+                            aria-label="Settings">
                         {item}
                         </ActionIcon>
                     ))}
                 </div>
             </div>
             <Divider my="xl"/>
-            <div className="[&_h4]:text_xl [&_*]:text-mine-shaft-300 [&_li]:marker:text-bright-sun-400 [&_li]:mb-1 [&_h4]:my-5 [&_h4]:font-semibold [&_h4]:text-mine-shaft-200 [&_p]:text-justify" dangerouslySetInnerHTML={{__html:data}}></div>
+            <div 
+                dangerouslySetInnerHTML={{__html:data}}
+                className={`
+                    [&_h4]:text_xl 
+                    [&_*]:text-mine-shaft-300 
+                    [&_li]:marker:text-bright-sun-400 
+                    [&_li]:mb-1 
+                    [&_h4]:my-5 
+                    [&_h4]:font-semibold 
+                    [&_h4]:text-mine-shaft-200 
+                    [&_p]:text-justify 
+                `}
+            >
+            </div>
             <Divider my="xl"/>
             <div>
                 <div className="text-xl font-semibold mb-5">
@@ -70,7 +96,11 @@ const JobDescription = (props:any) => {
                         <Button color="bright-sun.4" size="sm" variant="light">Company Page</Button>
                     </Link>
                 </div>
-                <div className="text-mine-shaft-300 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vel fugit illum perferendis totam! Fugit id magni assumenda earum minus ipsam quaerat provident, vel magnam mollitia libero nemo. Recusandae rerum dolore excepturi, consequuntur accusantium ducimus error accusamus nobis at sapiente!</div>
+                <div className="text-mine-shaft-300 text-justify">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vel fugit illum perferendis totam! 
+                    Fugit id magni assumenda earum minus ipsam quaerat provident, vel magnam mollitia libero nemo. 
+                    Recusandae rerum dolore excepturi, consequuntur accusantium ducimus error accusamus nobis at sapiente!
+                </div>
             </div>
         </div>
     );
