5bba7c305329c1d70f723a7464e59e96f0f90cb2
gsmkev <gsmkev@gmail.com>
2024-12-20 20:17:34 -0300

    style: enhance readability of Footer component code structure

--- frontend/src/Footer/Footer.tsx
+++ frontend/src/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className={'pt-20 pb-5 flex gap-5 justify-around bg-mine-shaft-950 font-[poppins]'}>
             <div className={'w-1/4 flex flex-col gap-4'}>
                 <div className={'flex gap-1 items-center text-bright-sun-400'}>
-                    <IconAsset className={'h-6 w-6'} stroke={1.25}/>
+                    <IconAsset className={'h-6 w-6'} stroke={1.25} />
                     <div className={'text-xl font-semibold'}>JobFinder</div>
                 </div>
                 <div className={'text-sm text-mine-shaft-300'}>
@@ -20,16 +20,29 @@ const Footer = () => {
                     <div><IconBrandX /></div>
                 </div>
             </div>
-            {footerLinks.map((item, index) => (
-                <div key={index} className={''}>
-                   <div className={'text-lg font-semibold mb-4 text-bright-sun-400'}>{item.title}</div>
-                   {item.links.map((link, index) => (
-                        <div key={index} className={'text-mine-shaft-300 text-sm hover:text-bright-sun-400 cursor-pointer mb-1 hover:translate-x-2 transition duration-300 ease-in-out'}>
-                            {link}
-                        </div>
-                    ))}
-                </div>
-            ))}
+            {
+                footerLinks.map((item, index) => (
+                    <div key={index} className={''}>
+                        <div className={'text-lg font-semibold mb-4 text-bright-sun-400'}>{item.title}</div>
+                        {item.links.map((link, index) => (
+                            <div
+                                key={index}
+                                className={`
+                                    text-mine-shaft-300 
+                                    text-sm 
+                                    hover:text-bright-sun-400 
+                                    cursor-pointer 
+                                    mb-1 
+                                    hover:translate-x-2 
+                                    transition 
+                                    duration-300 
+                                    ease-in-out`
+                                }>
+                                {link}
+                            </div>
+                        ))}
+                    </div>
+                ))}
         </div>
     );
 }
